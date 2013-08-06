b86bb216b773637bc5092754133fd424336cf5bc
Henrique Rodrigues <hsr@cs.ucsd.edu>
2013-08-05 22:09:49 -0700

    Extending SDNLink.java with a method that extracts links from a JSON object
    The new method added to SDNLink class extract links from a serialized JSON
    array object in the format exported by Floodlight.
    
    TODO:
    Fix maven dependencies to build the new SDNLink object.

--- src/main/java/net/es/oscars/pss/sdn/common/SDNLink.java
+++ src/main/java/net/es/oscars/pss/sdn/common/SDNLink.java
@@ -1,23 +1,29 @@
 package net.es.oscars.pss.sdn.common;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.ogf.schema.network.topology.ctrlplane.CtrlPlaneHopContent;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonToken;
+import com.fasterxml.jackson.databind.MappingJsonFactory;
+
 import net.es.oscars.utils.topology.NMWGParserUtil;
 
 public class SDNLink {
 	private SDNNode node;
-	
-	// TODO: add getters and setters	
-	public String srcNode;
-	public String dstNode;
-	public String srcPort;
-	public String dstPort;
-	public String srcLink;
-	public String dstLink; 
-	
+
+	// TODO: add getters and setters
+	public String srcNode = null;
+	public String dstNode = null;
+	public String srcPort = null;
+	public String dstPort = null;
+	public String srcLink = null;
+	public String dstLink = null;
+
 	public SDNNode getNode() {
 		return node;
 	}
@@ -26,6 +32,41 @@ public class SDNLink {
 		this.node = node;
 	}
 
+	/**
+	 * Checks if all the attributes are set.	
+	 * @return
+	 */
+	public boolean isComplete() {
+		return (this.srcNode != null) && (this.dstNode != null)
+				&& (this.srcPort != null) && (this.dstPort != null)
+				&& (this.srcLink != null) && (this.dstLink != null);
+	}
+
+	/**
+	 * Fill link attributes if they are still set to null. This is useful when
+	 * the application using this class don't consider link attributes on links
+	 * as OSCARS do
+	 */
+	private void fillLinkAttributes() {
+		if (this.srcLink == null)
+			this.srcLink = "1";
+		if (this.dstLink == null)
+			this.dstLink = "1";
+	}
+
+	/**
+	 * Empty constructor
+	 */
+	public SDNLink() {}
+
+	/**
+	 * Construct link from two URNs
+	 * 
+	 * @param srcURN
+	 *            URN that describes source of the link
+	 * @param dstURN
+	 *            URN that describes destination of the link
+	 */
 	public SDNLink(String srcURN, String dstURN) {
 		this.srcNode = NMWGParserUtil.getURNPart(srcURN, NMWGParserUtil.NODE_TYPE);
 		this.dstNode = NMWGParserUtil.getURNPart(dstURN, NMWGParserUtil.NODE_TYPE);
@@ -34,46 +75,160 @@ public class SDNLink {
 		this.srcLink = NMWGParserUtil.getURNPart(srcURN, NMWGParserUtil.LINK_TYPE);
 		this.dstLink = NMWGParserUtil.getURNPart(dstURN, NMWGParserUtil.LINK_TYPE);
 	}
-	
+
 	/**
-	 * Extract links from a CtrlPlaneHopContent object and returns them as a list of SDNLinks
+	 * Extract links from a CtrlPlaneHopContent object and returns them as a
+	 * list of SDNLinks
 	 * 
 	 * @param hops
 	 * @return list of SDNLinks
 	 */
-    public static List<SDNLink> extractSDNLinks(List<CtrlPlaneHopContent> hops) {
-    	List<SDNLink> links = new ArrayList<SDNLink>();
-    	String src = null;
-   	
-        try {
-        	for (CtrlPlaneHopContent hop : hops) {
-	        	String dst  = hop.getLink().getId();
+	public static List<SDNLink> extractSDNLinks(List<CtrlPlaneHopContent> hops) {
+		List<SDNLink> links = new ArrayList<SDNLink>();
+		String src = null;
 
-	        	if (src == null) {
-	        		src = dst;
-	        		continue;
-	        	}
-	        	
-	        	if (NMWGParserUtil.compareURNPart(src, dst, NMWGParserUtil.NODE_TYPE)) {
-	        		SDNLink l = new SDNLink(src, dst);
+		try {
+			for (CtrlPlaneHopContent hop : hops) {
+				String dst = hop.getLink().getId();
 
-	        		// TODO: check for capabilities
-	        		// TODO: change the design to avoid having multiple objects representing the same node
-	        		l.setNode(new SDNNode(l.srcNode.replaceAll("\\.", ":"))); 
-	        		links.add(l);
-	        	}
-	        	
-	        	src = dst;
-	        	
+				if (src == null) {
+					src = dst;
+					continue;
+				}
 
-	        }
+				if (NMWGParserUtil.compareURNPart(src, dst,
+						NMWGParserUtil.NODE_TYPE)) {
+					SDNLink l = new SDNLink(src, dst);
 
-        }
-        catch (Exception e) {
-        	return null; 
-        }
-        
+					// TODO: check for capabilities
+					// TODO: change the design to avoid having multiple objects
+					// representing the same node
+					l.setNode(new SDNNode(l.srcNode.replaceAll("\\.", ":")));
+					links.add(l);
+				}
+				src = dst;
+			}
+		} catch (Exception e) {
+			return null;
+		}
+
 		return links;
-    }
-	
+	}
+
+	/**
+	 * Extract links from a serialized JSON array object and returns them as a
+	 * list of SDNLinks
+	 * 
+	 * @param fmJson
+	 *            an serialized JSON array object with link descriptions
+	 * @return list of SDNLinks
+	 * @throws IOException
+	 */
+	public static List<SDNLink> extractSDNLinksFromJson(String fmJson)
+			throws IOException {
+		List<SDNLink> links = new ArrayList<SDNLink>();
+		SDNLink link = null;
+
+		MappingJsonFactory f = new MappingJsonFactory();
+		JsonParser jp;
+
+		try {
+			jp = f.createJsonParser(fmJson);
+		} catch (JsonParseException e) {
+			throw new IOException(e);
+		}
+
+		jp.nextToken();
+		if (jp.getCurrentToken() != JsonToken.START_ARRAY) {
+			throw new IOException("Expected START_ARRAY");
+		}
+
+		while (jp.nextToken() != JsonToken.END_ARRAY) {
+			if (jp.getCurrentToken() == JsonToken.START_OBJECT) {
+				if (link != null) {
+					link.fillLinkAttributes();
+
+					if (link.isComplete()) {
+						links.add(link);
+					} else {
+						throw new IOException("Link is incomplete");
+					}
+				}
+				link = new SDNLink();
+				jp.nextToken();
+			}
+
+			if (jp.getCurrentToken() == JsonToken.END_OBJECT) {
+				continue;
+			}
+
+			if (jp.getCurrentToken() != JsonToken.FIELD_NAME) {
+				System.out.println(jp.getCurrentToken());
+				throw new IOException("Expected FIELD_NAME");
+			}
+
+			String n = jp.getCurrentName();
+			jp.nextToken();
+			if (jp.getText().equals(""))
+				continue;
+
+			if (n == "src-switch")
+				link.setSrcNode(jp.getText());
+			else if (n == "dst-switch")
+				link.setDstNode(jp.getText());
+			else if (n == "src-port")
+				link.setSrcPort(jp.getText());
+			else if (n == "dst-port")
+				link.setDstPort(jp.getText());
+			else if (n == "direction" || n == "type") // ignore direction and
+														// type for now
+				jp.getText(); // do we have to call this to advance the head?
+		}
+
+		return links;
+	}
+
+	public void setSrcNode(String srcNode) {
+		this.srcNode = srcNode;
+	}
+
+	public String getDstNode() {
+		return dstNode;
+	}
+
+	public void setDstNode(String dstNode) {
+		this.dstNode = dstNode;
+	}
+
+	public String getSrcPort() {
+		return srcPort;
+	}
+
+	public void setSrcPort(String srcPort) {
+		this.srcPort = srcPort;
+	}
+
+	public String getDstPort() {
+		return dstPort;
+	}
+
+	public void setDstPort(String dstPort) {
+		this.dstPort = dstPort;
+	}
+
+	public String getSrcLink() {
+		return srcLink;
+	}
+
+	public void setSrcLink(String srcLink) {
+		this.srcLink = srcLink;
+	}
+
+	public String getDstLink() {
+		return dstLink;
+	}
+
+	public void setDstLink(String dstLink) {
+		this.dstLink = dstLink;
+	}
 }
